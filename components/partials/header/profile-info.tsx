f40be443b162a1c05d337366c6239b3796044a67
Ahmed Khaled <ahmedd.khaled50@gmail.com>
2026-05-03 17:15:23 +0300

    feat: add ProfileInfo component with sign-out functionality

--- components/partials/header/profile-info.tsx
+++ components/partials/header/profile-info.tsx
@@ -30,48 +30,16 @@ const ProfileInfo = () => {
 
   return (
     <div className="block">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild className=" cursor-pointer">
-          <div className=" flex items-center gap-3  text-default-800 ">
-            <span className="text-base  me-2.5 inline-block">
-              <Icon icon="heroicons-outline:chevron-down"></Icon>
-            </span>
-          </div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56 p-0" align="end">
-          <DropdownMenuGroup>
-            {[
-              {
-                name: "Settings",
-                icon: "heroicons:paper-airplane",
-                href: "/dashboard/settings"
-              },
-            ].map((item, index) => (
-              <Link
-                href={item.href}
-                key={`info-menu-${index}`}
-                className="cursor-pointer"
-              >
-                <DropdownMenuItem className="flex items-center gap-2 text-sm font-medium text-default-600 capitalize px-3 py-1.5 cursor-pointer">
-                  <Icon icon={item.icon} className="w-4 h-4" />
-                  {item.name}
-                </DropdownMenuItem>
-              </Link>
-            ))}
-          </DropdownMenuGroup>
-          <DropdownMenuSeparator />
-          <DropdownMenuSeparator className="mb-0 dark:bg-background" />
-          <DropdownMenuItem
+          <div
               onClick={handleSignOut}
             className="flex items-center gap-2 text-sm font-medium text-default-600 capitalize my-1 px-3 cursor-pointer"
           >
-            <button type="button" className=" w-full  flex  items-center gap-2" >
+            <button type="button" className=" w-full  flex  items-center gap-2 cursor-pointer" >
               <Icon icon="heroicons:power" className="w-4 h-4" />
               Log out
             </button>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+          </div>
+
     </div>
   );
 };
